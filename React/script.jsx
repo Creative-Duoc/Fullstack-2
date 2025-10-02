68c7a532b6f23662de86f7acceea1b096f0b3f71
Creative-Duoc <sam.bueno@duocuc.cl>
2025-10-01 21:59:17 -0300

    Intente hacer algo, lo logre? no

--- React/script.jsx
+++ React/script.jsx
@@ -1,34 +1,26 @@
+
+let frutas = ["banana","Fresa"]
 function Header(prop) {
-    const name = prop.nombre
-    const apellido = prop.apellido
-  return <h1>Develop. Preview. {name} {apellido}</h1>;
+  return <h1>Develop. Preview. </h1>;
 }
  
 function HomePage() {
-    const [likes,setlikes] = React.useState(0)
-    function handleClick(){
-        setlikes(likes+1)
-    }
-    function restarLike(){
-        
-        if (likes <= 0){
-            likes = 0
-        }
-        else {
-            setlikes(likes-1)
-        }
+
+    const[div1, escribirDiv] = React.useState( 0 )
+    function agregarFruta (){
+        escribirDiv(div1+1)
     }
 
-    
-  return (
-    <div>
-        <button onClick = {handleClick}>Suma</button>
-        <button onClick = {restarLike}>Resta </button>
-        <p>Tienes {likes} likes</p>
-      <Header nombre = "nombre" apellido = "apellido"/>
-      <Header nombre/>
-    </div>
-  );
+    return (
+        
+        <div>
+            <button onClick = {agregarFruta}>Suma</button>
+            <h1>{div1}</h1>
+            
+            <Header/>
+        </div>
+    )
+
 }
  
 const root = ReactDOM.createRoot(app);
